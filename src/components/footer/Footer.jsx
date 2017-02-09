4d88b7a37e878e9d1ed8bb05f5deb56fe1dfda4b
Yassine <elbayass59@gmail.com>
2017-02-09 11:26:06 +0100

    debut du footer

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,15 +1,26 @@
 import React from 'react';
-
+import css from './Footer.css'
+var SocialButton = require('react-social-button');
 class Footer extends React.Component {
 
 	render() {
 		return (
-			<div>
-
-				<p>Footer</p>
+<div>
 
 
-			</div>
+	<SocialButton
+	  social='twitter'
+	  btnProps={{
+	    disabled: true,
+	    onClick: function(){alert('Callback called.');}
+	  }}/>
+	<div class="footerGauche" style={{float:"right"}}>
+		<a href="#">Accueil</a>
+		<a href="#">Carte</a>
+		<a href="#">Contact</a>
+	</div>
+</div>
+
 			);
 	}
 }
