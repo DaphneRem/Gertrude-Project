4dba67efba55793734f7908c72d39c1c9c23aee3
daphne remali <remalidaphne@gmail.com>
2017-02-13 12:36:09 +0100

    add responsive and css

--- src/components/googleMap/GoogleMap.jsx
+++ src/components/googleMap/GoogleMap.jsx
@@ -4,9 +4,9 @@ class GoogleMap extends React.Component {
 	render() {
 		return (
 
-<div>
+<div className="map" style={{display:"flex",justifyContent:"center"}}>
 	<iframe
-	  frameBorder="0" style={{border:0,width:"100%",height:"25px",zoom:9}}
+	  frameBorder="0" style={{border:0,width:"90%",height:"25px",margin:"2px",zoom:9}}
 	  src="https://www.google.com/maps/embed/v1/place?key=AIzaSyDyn4n9NbE91_kcahkbnUKGPYoEca7LEMg&q=55+rue+de+vincennes,Montreuil+FR" allowFullScreen>
 	</iframe>
 
--- src/components/presentation/Presentation.jsx
+++ src/components/presentation/Presentation.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import contenu from '../data/data.js';
+import './Presentation.css';
 
 class Presentation extends React.Component {
 
 	render() {
+
 		return (
-			<div>
+			<div className="presentation" id="presentation">
 				<h1>{contenu.name}</h1>
 				<p>{contenu.description}</p>
 			</div>
